fb95708a3ee1ac5f3ddec942a1830473322ed361
jacksmithinsulander <666.jack.smith@protonmail.com>
2023-11-04 22:16:31 +0100

    final fix for javadocs

--- src/Lending.sol
+++ src/Lending.sol
@@ -5,12 +5,9 @@ pragma solidity ^0.8.19;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPriceFetcher} from "./interfaces/IPriceFetcher.sol";
 
-/**
- * @title Lending
- * @author CoreFi-Cash Technical Team
- * @notice Lending contract
- *
- */
+/// @title Lending
+/// @author CoreFi-Cash Technical Team
+/// @notice Lending contract
 contract Lending {
     /// @dev nonce identifier for lender
     uint256 public nonceLending;
@@ -22,14 +19,9 @@ contract Lending {
     /// @dev PriceFetcher contract interface
     IPriceFetcher immutable priceFetcherLending;
 
-    /**
-     * @notice
-     *  Lending Constructor
-     *
-     * @param _USDT USDT contract address
-     * @param _oracle Oracle for core-price contract address
-     *
-     */
+    /// @notice Lending Constructor
+    /// @param _USDT USDT contract address
+    /// @param _oracle Oracle for core-price contract address
     constructor(address _USDT, address _oracle) {
         nonceLending = 0;
         USDT_Lending = IERC20(_USDT);
@@ -66,13 +58,8 @@ contract Lending {
         _;
     }
 
-    /**
-     * @notice
-     *  Let a user deposit funds as a lender
-     *
-     * @param _newDepositAmount Amount of funds thAmount of funds that the user wants to deposit
-     *
-     */
+    /// @notice Let a user deposit funds as a lender
+    /// @param _newDepositAmount Amount of funds thAmount of funds that the user wants to deposit
     function depositFunds(uint256 _newDepositAmount) public {
         require(_newDepositAmount > 0, "Can't deposit 0 funds");
         userDepositedAmount[msg.sender] += _newDepositAmount;
@@ -83,13 +70,8 @@ contract Lending {
         USDT_Lending.transferFrom(msg.sender, address(this), _newDepositAmount);
     }
 
-    /**
-     * @notice
-     *  Lets a lender withdraw their funds from the borrow pool
-     *
-     * @param _amount Amount they want to withdraw
-     *
-     */
+    /// @notice Lets a lender withdraw their funds from the borrow pool
+    /// @param _amount Amount they want to withdraw
     function withdrawFunds(uint256 _amount) public {
 
         require(userDepositedAmount[msg.sender] >= _amount && USDT_Lending.balanceOf(address(this)) > _amount, "Amount of funds deposited is not enough");
@@ -98,26 +80,15 @@ contract Lending {
         USDT_Lending.transfer(msg.sender, _amount);
     }
 
-    /**
-     * @notice
-     *  Let a user check their current active lending positions
-     *
-     * 
-     * @return _position returning the position
-     *
-     */
+    /// @notice Let a user check their current active lending positions
+    /// @return _position returning the position
     function activeLendPosition() public view returns(uint256 _position) {
         _position = userDepositedAmount[msg.sender];
     }
 
-    /**
-     * @notice
-     *  Private function to compute the current yield
-     *
-     * @param _deposit deposition params
-     *
-     * @return _yieldInCore Amount of yield the user can claim
-     */
+    /// @notice Private function to compute the current yield
+    /// @param _deposit deposition params
+    /// @return _yieldInCore Amount of yield the user can claim
     function computeYield(Deposit memory _deposit) private returns(uint256 _yieldInCore) {
         uint256 timestamp = block.timestamp - _deposit.timestamp;
 
@@ -126,14 +97,8 @@ contract Lending {
         _yieldInCore = _yield / uint256(priceFetcherLending.fetchLatestResult());
     }
 
-    /**
-     * @notice
-     *  Let a user check their claimable yield
-     *
-     * 
-     * @return _total returning the total amounht claimable
-     *
-     */
+    /// @notice Let a user check their claimable yield
+    /// @return _total returning the total amounht claimable
     function getInterestEarnings() public returns(uint256 _total) {
         _total = 0;
         for(uint256 i = 0; i < deposits[msg.sender].length; i++) {
