2f1232d4d449685ba6beeb6a4163d6527cc04c6f
bidetaggle <bidetaggle@gmail.com>
2023-11-04 15:27:14 +0000

    uncomment test

--- test/Master.t.sol
+++ test/Master.t.sol
@@ -1,33 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0
-// pragma solidity ^0.8.20;
+pragma solidity ^0.8.20;
 
-// import "forge-std/Test.sol";
+import "forge-std/Test.sol";
 
-// import {ERC20Mock} from "./mock/erc20.sol";
-// import {Master} from "../src/Master.sol";
+import {ERC20Mock} from "./mock/erc20.sol";
+import {Master} from "../src/Master.sol";
 
-// // USDT address (mainnet): 0x900101d06A7426441Ae63e9AB3B9b0F63Be145F1
+// USDT address (mainnet): 0x900101d06A7426441Ae63e9AB3B9b0F63Be145F1
 
-// contract MasterTest is Test {
-//     ERC20Mock USDT;
-//     Master master;
+contract MasterTest is Test {
+    ERC20Mock USDT;
+    Master master;
 
-//     address alice = makeAddr("alice");
+    address alice = makeAddr("alice");
 
-//     function setUp() public {
-//         USDT = new ERC20Mock("USDT", "USDT");
-//         deal(address(USDT), alice, 200);
+    function setUp() public {
+        USDT = new ERC20Mock("USDT", "USDT");
+        deal(address(USDT), alice, 200);
 
-//         master = new Master(address(USDT));
-//     }
+        master = new Master(address(USDT), address(1));
+    }
 
-//     function testDeposit() public {
-//         vm.startPrank(alice);
+    function testDeposit() public {
+        vm.startPrank(alice);
 
-//         USDT.approve(address(master), 100);
-//         master.depositFunds(100);
+        USDT.approve(address(master), 100);
+        master.depositFunds(100);
 
-//         assertEq(USDT.balanceOf(address(alice)), 100);
-//         assertEq(USDT.balanceOf(address(master)), 100);
-//     }
-// }
+        assertEq(USDT.balanceOf(address(alice)), 100);
+        assertEq(USDT.balanceOf(address(master)), 100);
+    }
+}
