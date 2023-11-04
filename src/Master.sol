9319110060db29e8fdb353f1fa3c05efd0c232fd
bidetaggle <bidetaggle@gmail.com>
2023-11-04 10:40:20 +0000

    fix undeclared identifier

--- src/Master.sol
+++ src/Master.sol
@@ -9,6 +9,8 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Master is Borrow, Lending{
     /// @dev USDC contract interface
+
+    IERC20 public immutable USDT;
     
     constructor(address _USDT) Borrow(_USDT) Lending(_USDT) {
         USDT = IERC20(_USDT);
